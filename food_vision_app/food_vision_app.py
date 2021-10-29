3bbbfc3971f1361429528e4c3932f8b75d41389f
Logan Clark <logan.clark@wsu.edu>
2021-10-29 16:55:50 -0700

    Creating directery for food vision app

---
+++ food_vision_app/food_vision_app.py
@@ -0,0 +1,20 @@
+# Logan Clark 10/29/2021
+# App for running a Food Vision model
+# Built with streamlit
+
+
+
+
+
+
+
+
+# External files to download
+EXTERNAL_DEPENDENCIES = {
+    "food_vision.weights": {
+        "url": ""
+    }
+}
+
+if __name__ == "__main__":
+    main()
